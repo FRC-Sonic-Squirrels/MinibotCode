b40961540056cefde5d18a2bb9f2beb0efb924c1
LuminousLlama <keyushattarde@gmail.com>
2022-01-14 20:29:32 -0800

    Working blue red cargo 2022

--- src/main/java/frc/robot/subsystems/colorSensor.java
+++ src/main/java/frc/robot/subsystems/colorSensor.java
@@ -28,17 +28,17 @@ public class colorSensor extends SubsystemBase {
    * Color Wheel Blue CMY: 100,0,0 RGB: #00FFFF Green CMY: 100,0,100 RGB: #00FF00
    * Red CMY: 0,100,100 RGB: #FF0000 Yellow CMY: 0,0,100 RGB: #FFFF00
    */
-  private final Color kBlueTarget = ColorMatch.makeColor(0.143, 0.427, 0.429);
-  private final Color kGreenTarget = ColorMatch.makeColor(0.197, 0.561, 0.240);
-  private final Color kRedTarget = ColorMatch.makeColor(0.561, 0.232, 0.114);
-  private final Color kYellowTarget = ColorMatch.makeColor(0.361, 0.524, 0.113);
+  private final Color kBlueTarget = ColorMatch.makeColor(0.0, 0.5, 0.5);
+  //private final Color kGreenTarget = ColorMatch.makeColor(0.197, 0.561, 0.240);
+  private final Color kRedTarget = ColorMatch.makeColor(0.5454, 0.0908, 0.36352);
+  //private final Color kYellowTarget = ColorMatch.makeColor(0.361, 0.524, 0.113);
 
   public colorSensor() {
     // colors we want to match
     m_colorMatcher.addColorMatch(kBlueTarget);
-    m_colorMatcher.addColorMatch(kGreenTarget);
+    //m_colorMatcher.addColorMatch(kGreenTarget);
     m_colorMatcher.addColorMatch(kRedTarget);
-    m_colorMatcher.addColorMatch(kYellowTarget);
+    //m_colorMatcher.addColorMatch(kYellowTarget);
 
     SmartDashboard.putNumber("Red", 0.0);
     SmartDashboard.putNumber("Green", 0.0);
@@ -65,19 +65,19 @@ public class colorSensor extends SubsystemBase {
     /**
      * Run the color match algorithm on our detected color
      */
-    String colorString;
+    String colorString = "";
     ColorMatchResult match = m_colorMatcher.matchClosestColor(detectedColor);
 
     if (match.color == kBlueTarget) {
       colorString = "Blue";
     } else if (match.color == kRedTarget) {
       colorString = "Red";
-    } else if (match.color == kGreenTarget) {
-      colorString = "Green";
-    } else if (match.color == kYellowTarget) {
-      colorString = "Yellow";
-    } else {
-      colorString = "Unknown";
+    // } else if (match.color == kGreenTarget) {
+    //   colorString = "Green";
+    // } else if (match.color == kYellowTarget) {
+    //   colorString = "Yellow";
+    // } else {
+    //   colorString = "Unknown";
     }
 
     SmartDashboard.putNumber("Red", detectedColor.red);
@@ -92,35 +92,35 @@ public class colorSensor extends SubsystemBase {
 
     /* Code for counting colors */
 
-    if (lastSeenColor.equals("Red")) {
-      if (colorString.equals("Green")) {
-        count = count + 1;
-      }
-      if (colorString.equals("Yellow")) {
-        count = count - 1;
-      }
-    } else if (lastSeenColor.equals("Green")) {
-      if (colorString.equals("Blue")) {
-        count = count + 1;
-      }
-      if (colorString.equals("Red")) {
-        count = count - 1;
-      }
-    } else if (lastSeenColor.equals("Blue")) {
-      if (colorString.equals("Yellow")) {
-        count = count + 1;
-      }
-      if (colorString.equals("Green")) {
-        count = count - 1;
-      }
-    } else if (lastSeenColor.equals("Yellow")) {
-      if (colorString.equals("Red")) {
-        count = count + 1;
-      }
-      if (colorString.equals("Blue")) {
-        count = count - 1;
-      }
-    }
+    // if (lastSeenColor.equals("Red")) {
+    //   if (colorString.equals("Green")) {
+    //     count = count + 1;
+    //   }
+    //   if (colorString.equals("Yellow")) {
+    //     count = count - 1;
+    //   }
+    // } else if (lastSeenColor.equals("Green")) {
+    //   if (colorString.equals("Blue")) {
+    //     count = count + 1;
+    //   }
+    //   if (colorString.equals("Red")) {
+    //     count = count - 1;
+    //   }
+    // } else if (lastSeenColor.equals("Blue")) {
+    //   if (colorString.equals("Yellow")) {
+    //     count = count + 1;
+    //   }
+    //   if (colorString.equals("Green")) {
+    //     count = count - 1;
+    //   }
+    // } else if (lastSeenColor.equals("Yellow")) {
+    //   if (colorString.equals("Red")) {
+    //     count = count + 1;
+    //   }
+    //   if (colorString.equals("Blue")) {
+    //     count = count - 1;
+    //   }
+    // }
 
     // TODO: remove debug print
     if (!lastSeenColor.equals(colorString)) {
